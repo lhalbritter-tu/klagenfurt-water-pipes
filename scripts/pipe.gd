cac08f6b74aa04291c0abf0bdc522e057e0d2cdb
lhalbritter-tu <leo.halbritter@hotmail.com>
2024-12-04 16:47:59 +0100

    Base Pipe System

--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var rotate_by_degrees = 90
+@export var rigidbody: RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	pass	
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT :
+			print("Left mouse button")
+			rotate_right()
+		if event.button_index == MOUSE_BUTTON_RIGHT :
+			print("Right mouse button")
+			rotate_left()
+
+func rotate_right():
+	rigidbody.rotate(deg_to_rad(rotate_by_degrees))
+	
+func rotate_left():
+	rigidbody.rotate(deg_to_rad(-rotate_by_degrees))
